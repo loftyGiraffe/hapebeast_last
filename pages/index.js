f42b933cde05fd1153f32cd39dc2876d95a96a5f
loftyGiraffe <94640313+loftyGiraffe@users.noreply.github.com>
2022-01-15 23:23:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -147,7 +147,7 @@ export default function Home() {
                         fontWeight: '500',
                     }}
                     min="1"
-                    max="6"
+                    max="10"
                     type="number"
                 />
                 <div style={{ color: "white", fontSize: "30px", fontWeight: '500', }}>Hapebeast(s)</div>
